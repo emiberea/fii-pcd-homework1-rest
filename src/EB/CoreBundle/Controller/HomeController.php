f9a51fb777a15c7032ab72b794a8093b30b12199
Emi Berea <emi.berea@gmail.com>
2015-08-01 23:30:45 +0300

    updates

--- src/EB/CoreBundle/Controller/HomeController.php
+++ src/EB/CoreBundle/Controller/HomeController.php
@@ -2,11 +2,9 @@
 
 namespace EB\CoreBundle\Controller;
 
-use EB\RideBundle\Entity\RideRequestStatus;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Doctrine\ORM\EntityManager;
 
 class HomeController extends Controller
 {
